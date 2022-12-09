197c5e5a80ddcd0260dbbb7a56f383268207a58e
behnamsaba <behnams71@gmail.com>
2022-12-08 18:26:43 -0800

    fix bug

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 """Blogly application."""
 from flask import Flask,render_template,request,redirect
-from models import db, connect_db
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db,connect_db, User
 
 app = Flask(__name__)
-app.app_context().push()
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,7 +14,8 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
